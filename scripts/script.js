b472c318515a45a63634bcb75dced770c42ab00f
Shahriyor <shahriyordeveloping@gmail.com>
2021-07-27 04:55:27 +0300

    initial

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,10 @@ function dropdown(val) {
   var y = document.getElementsByClassName('dropdown-toggle');
   var aNode = y[0].innerHTML = val + ' <span class="caret"></span>'; // Append 
 }
+
+$(function() {
+  $('.limited').click(function() {
+    $('.limited').not(this).removeClass('active')
+    $(this).toggleClass('active')
+  })
+})
